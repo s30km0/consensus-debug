95e31e368431f43eaaa5db6f2fd1bb9c692d1849
Noah S. Kim <s30km0@outlook.com>
2021-10-31 10:28:33 +0900

    remove datadir's output

--- attach.sh
+++ attach.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 $GOPATH/src/github.com/ethereum/go-ethereum/build/bin/geth attach miner-node/geth.ipc
+
+# > personal.newAccount("1q2w3e4r!")
+# > eth.coinbase
+# > eth.getBalance("0x37cd0920b2c7a2a2fc5a143e009cad218f5e9ec6")
+# > eth.mining
+# > miner.start(1)
+
--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf miner-node
+rm -rf validator-node
+
 $GOPATH/src/github.com/ethereum/go-ethereum/build/bin/geth init genesis.json --datadir=miner-node
 
 $GOPATH/src/github.com/ethereum/go-ethereum/build/bin/geth init genesis.json --datadir=validator-node
